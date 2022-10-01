f1f4ff08c9b3fa030421741b23713fa3e8edcd8f
Sylvia Cung <scung@blend.com>
2022-09-30 17:48:18 -0700

    hmm

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,10 @@ const htmlPlugin = new HtmlWebPackPlugin({
 const __dirname = path.resolve();
 
 export default {
-  entry: "./src/index.tsx",
+  entry: "/src/index.tsx",
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "/[name].js",
+    filename: "[name].js",
   },
   devServer: {
     static: "./dist",
